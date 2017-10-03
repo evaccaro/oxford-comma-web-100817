4eb126b367aaba36b3c3c1e1711fd2da5a567077
Elisa Vaccaro <erv230@nyu.edu>
2017-10-03 00:30:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
-temp = array[0, -1]
-temp
+  answer = ""
+  if array.length == 1
+    answer = array[0]
+  else
+    answer = "#{array[0, array.length].join(", ")} and #{array[-1]}"
+  
 end
