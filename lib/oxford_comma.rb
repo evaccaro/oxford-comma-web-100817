50d8336898c3817f7b159650e30cbda10c2b1a0e
Elisa Vaccaro <erv230@nyu.edu>
2017-10-03 00:30:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 1
     answer = array[0]
   else
-    answer = "#{array[0, array.length].join(", ")} and #{array[-1]}"
+    answer = "#{array[0, array.length-1].join(", ")} and #{array[-1]}"
   end
   answer
 end
